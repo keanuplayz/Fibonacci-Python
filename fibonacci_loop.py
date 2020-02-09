a7014d4dd14905c6bef8983588b50922cb74d6c1
Keanu <keanutimmermans8@gmail.com>
2020-02-09 20:36:43 +0100

    Explained my code in Dutch.

--- fibonacci_loop.py
+++ fibonacci_loop.py
@@ -1,10 +1,12 @@
-# Enter number of terms needed                   #0,1,1,2,3,5....
-InputNumber=int(input("Enter the terms: "))
-FirstElement=0                                         #first element of series
-SecondElement=1                                         #second element of series
-if InputNumber<=0:
+InputNumber=int(input("Enter the terms: "))                     # Voer hier het aantal Fibonacci nummers in
+                                                                # Het aantal Fibonacci nummers die worden weergegeven hangen af van de input van InputNumber
+
+FirstElement=0                                                  # Definitie voor het eerste element
+SecondElement=1                                                 # Definitie voor het tweede element
+
+if InputNumber<=0:                                              # Als er een nummer onder de 0 wordt ingevoerd, print:
     print("The requested series is",FirstElement)
-else:
+else:                                                           # Anders, start de Fibonacci reeks met het aangegeven nummer bij InputNumer
     print(FirstElement,SecondElement,end=" ")
     for x in range(2,InputNumber):
         next=FirstElement+SecondElement                           
